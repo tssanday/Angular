7ce2df1a3372d4ebb434920e8679031398c5a1c7
tssanday <tssanday@usm.edu.ph~>
2022-06-10 00:19:33 +0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://sanday-bookstore.netlify.app';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://tssanday-001-site1.gtempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false
+  },
+  apis: {
+    default: {
+      url: 'http://tssanday-001-site1.gtempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
